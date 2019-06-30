b4de55d6a728a4156b254b48c21e65a94b56ead2
Rahul Jain <jainrahul0311@gmail.com>
2019-06-30 15:41:28 +0530

    removed 'var' from views.py as well as home.html

--- wordcount/views.py
+++ wordcount/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'home.html',{'var':'This is WorldCount from python code'})
+    return render(request,'home.html')
 
 def Gym(request):
     return HttpResponse('<h1>Lets Go To The Gym!!')
