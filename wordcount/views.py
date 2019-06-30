d5668c090b55d72787a42659ae0198b5549252ca
Rahul Jain <jainrahul0311@gmail.com>
2019-06-30 15:33:16 +0530

    added Python code and in home.html and updated view.py

--- wordcount/views.py
+++ wordcount/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'home.html',{'var':'This is WorldCount from python code'})
 
 def Gym(request):
     return HttpResponse('<h1>Lets Go To The Gym!!')
