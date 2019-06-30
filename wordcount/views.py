5b11ef751e36b246dbf24e7cf3ee0d38cab85094
Rahul Jain <jainrahul0311@gmail.com>
2019-06-30 15:10:23 +0530

    Added Templates 'home.html' and made changes in settings.py + Urls.py

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('<h1>Hello</h1>')
+    return render(request,'home.html')
 
 def Gym(request):
     return HttpResponse('<h1>Lets Go To The Gym!!')
