d9190e669a0699a5378162506271cc77f74a77aa
Rahul Jain <37879088+jainrahul0311@users.noreply.github.com>
2023-04-13 12:19:10 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks. Removed"""
+"""Django's command-line utility for administrative tasks. Removed1"""
 import os
 import sys
 
